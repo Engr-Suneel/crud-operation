f352451cba34cc344665471fec453bdce4883d18
laheera336 <heerakhatri24@gmail.com>
2022-07-26 20:58:22 +0500

    change request done

---
+++ src/app/shared/box-card/box-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-box-card',
+  templateUrl: './box-card.component.html',
+  styleUrls: ['./box-card.component.css']
+})
+export class BoxCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
